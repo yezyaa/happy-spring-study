d58949d2c4ed022870cd2ce61bb98c7b6eb5bcc2
ycyeon <ickite@naver.com>
2023-06-11 23:07:49 +0900

    create -> create-view redirect

--- src/main/java/com/happy/happycrud/yezy/BoardController.java
+++ src/main/java/com/happy/happycrud/yezy/BoardController.java
@@ -1,25 +1,25 @@
-package com.happy.happycrud.yezy;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class BoardController {
-    @GetMapping("/create-view")
-    public String createView() {
-        return "create";
-    }
-
-    @PostMapping("/create")
-    public String create(
-            @RequestParam("title") String title,
-            @RequestParam("content") String content,
-            @RequestParam("name") String name) {
-        System.out.println(title);
-        System.out.println(content);
-        System.out.println(name);
-        return "create";
-    }
-}
+//package com.happy.happycrud.yezy;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class BoardController {
+//    @GetMapping("/create-view")
+//    public String createView() {
+//        return "create";
+//    }
+//
+//    @PostMapping("/create")
+//    public String create(
+//            @RequestParam("title") String title,
+//            @RequestParam("content") String content,
+//            @RequestParam("name") String name) {
+//        System.out.println(title);
+//        System.out.println(content);
+//        System.out.println(name);
+//        return "create";
+//    }
+//}
--- src/main/java/com/happy/happycrud/yezy/model/BoardDto.java
+++ src/main/java/com/happy/happycrud/yezy/model/BoardDto.java
@@ -1,11 +1,50 @@
-package com.happy.happycrud.yezy.model;
-
-public class BoardDto {
-    private Long id; // 번호
-    private String title; // 제목
-    private String content; // 내용
-    private String name; // 작성자
-
-    public BoardDto() {
-    }
-}
+//package com.happy.happycrud.yezy.model;
+//
+//public class BoardDto {
+//    private Long id; // 번호
+//    private String title; // 제목
+//    private String content; // 내용
+//    private String name; // 작성자
+//
+//    public BoardDto() {
+//    }
+//
+//    public BoardDto(Long id, String title, String content, String name) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//        this.name = name;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
