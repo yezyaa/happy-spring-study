427a64fd1a84e89fe655a2f64b4287559c2d994e
yezy <iyeahji@gmail.com>
2023-06-11 20:17:05 +0900

    init

---
+++ src/main/java/com/happy/happycrud/HappyCrudApplication.java
@@ -0,0 +1,13 @@
+package com.happy.happycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyCrudApplication.class, args);
+	}
+
+}
