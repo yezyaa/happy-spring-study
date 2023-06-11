83c2647eb09d97a0cf2fbb0da346bb2387081881
ycyeon <ickite@naver.com>
2023-06-12 00:11:03 +0900

    readBoardAll 추가

--- src/main/java/com/happy/happycrud/chaeyeon/model/BoardDto.java
+++ src/main/java/com/happy/happycrud/chaeyeon/model/BoardDto.java
@@ -16,4 +16,36 @@
 //        this.content = content;
 //        this.name = name;
 //    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 //}
