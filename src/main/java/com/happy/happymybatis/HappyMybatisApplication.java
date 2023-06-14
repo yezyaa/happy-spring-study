7d1fea1071d40df17bdff6394ad4009271827935
yezy <iyeahji@gmail.com>
2023-06-14 21:07:09 +0900

    git init

---
+++ src/main/java/com/happy/happymybatis/HappyMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.happy.happymybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happy/happymybatis/HappyMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.happymybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
