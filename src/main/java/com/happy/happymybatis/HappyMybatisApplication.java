5e1cb6a52fcb23167178661da93c8b48ae48ffb8
yezy <iyeahji@gmail.com>
2023-06-15 19:18:01 +0900

    feat: XML로 SQL 작성 (최종)

--- src/main/java/com/happy/happymybatis/HappyMybatisApplication.java
+++ src/main/java/com/happy/happymybatis/HappyMybatisApplication.java
@@ -1,13 +1,30 @@
 package com.happy.happymybatis;
 
+import com.happy.happymybatis.yezy.dao.BoardDao;
+import com.happy.happymybatis.yezy.model.Board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HappyMybatisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HappyMybatisApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(HappyMybatisApplication.class, args);
+		BoardDao dao = applicationContext.getBean(BoardDao.class);
+
+		// SELECT
+//		System.out.println(dao.readBoardAll());
+//		System.out.println(dao.readBoard(1L));
+		System.out.println(dao.readAllXml());
+
+		// INSERT
+//		Board board = new Board();
+//		board.setTitle("제목 추가");
+//		board.setContent("내용 추가");
+//		board.setName("dave");
+//      dao.createBoard(board);
+//      System.out.println(dao.readBoardAll());
 	}
 
 }
