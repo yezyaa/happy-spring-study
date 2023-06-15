566c7e64befea395a58da14e192cd284b501ce3a
ycyeon <ickite@naver.com>
2023-06-15 19:20:25 +0900

    찐 최종

--- src/main/java/com/happy/happymybatis/HappyMybatisApplication.java
+++ src/main/java/com/happy/happymybatis/HappyMybatisApplication.java
@@ -1,13 +1,20 @@
 package com.happy.happymybatis;
 
+import com.happy.happymybatis.chaeyeon.dao.BoardDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HappyMybatisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HappyMybatisApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(HappyMybatisApplication.class, args);
+		BoardDao dao = applicationContext.getBean(BoardDao.class);
+
+		System.out.println(dao.readBoardAll());
+		System.out.println(dao.readAllXml());
+
 	}
 
 }
---
+++ src/main/java/com/happy/happymybatis/chaeyeon/mapper/BoardMapperX.java
@@ -0,0 +1,10 @@
+//package com.happy.happymybatis.chaeyeon.mapper;
+//
+//import com.happy.happymybatis.chaeyeon.model.Board;
+//
+//import java.util.List;
+//
+//public interface BoardMapperX {
+//    Board selectBoard(Long id);
+//    List<Board> selectBoardAll();
+//}
