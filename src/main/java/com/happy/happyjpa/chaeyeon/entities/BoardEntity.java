0f5ff3365148bb06d88aad8204e43f72a27dc7d5
ycyeon <ickite@naver.com>
2023-06-18 21:39:02 +0900

    entity 작성

---
+++ src/main/java/com/happy/happyjpa/chaeyeon/entities/BoardEntity.java
@@ -0,0 +1,17 @@
+//package com.happy.happyjpa.chaeyeon.entities;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Data
+//@Entity
+//@Table(name = "board")
+//public class BoardEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String title;
+//    private String content;
+////    @Column(name = "username", unique = true)
+//    private String name;
+//}
