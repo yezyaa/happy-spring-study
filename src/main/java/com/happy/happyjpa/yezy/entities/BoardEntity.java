2df4a14518e92078ec61a67c7f2ab8c015467384
ycyeon <ickite@naver.com>
2023-06-19 20:45:20 +0900

    충돌 ㄹㅇ 수정

--- src/main/java/com/happy/happyjpa/yezy/entities/BoardEntity.java
+++ src/main/java/com/happy/happyjpa/yezy/entities/BoardEntity.java
@@ -1,17 +1,17 @@
-//package com.happy.happyjpa.yezy.entities;
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//
-//@Data // Lombok
-//@Entity
-//@Table(name = "board") // 생성되는 테이블의 이름을 "board"로
-//public class BoardEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String title;
-//    private String content;
-////    @Column(name = "username", unique = true) // Unique Constraint 및 컬럼 이름 지정
-//    private String name;
-//}
+package com.happy.happyjpa.yezy.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data // Lombok
+@Entity
+@Table(name = "board") // 생성되는 테이블의 이름을 "board"로
+public class BoardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+//    @Column(name = "username", unique = true) // Unique Constraint 및 컬럼 이름 지정
+    private String name;
+}
--- src/main/java/com/happy/happyjpa/yezy/repos/BoardRepository.java
+++ src/main/java/com/happy/happyjpa/yezy/repos/BoardRepository.java
@@ -1,15 +1,15 @@
-//package com.happy.happyjpa.yezy.repos;
-//
-//import com.happy.happyjpa.yezy.entities.BoardEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//// 데이터베이스와 소통하기 위한 창구
-//public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-//    // JpaRepository<내가 사용할 Entity, 그 Entity의 ID 컬럼의 타입>
-//    // Spring Data JPA = Query Method
-//    // 메소드 이름을 우리가 조회하고 싶은 조건을 붙여서 만든다.
-//    // 하나 또는 많이
-//    // (findBy || findAllBy) + [(Column + 조건) * n] + [OrderBy + Column]
-//
-//
-//}
+package com.happy.happyjpa.yezy.repos;
+
+import com.happy.happyjpa.yezy.entities.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 데이터베이스와 소통하기 위한 창구
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    // JpaRepository<내가 사용할 Entity, 그 Entity의 ID 컬럼의 타입>
+    // Spring Data JPA = Query Method
+    // 메소드 이름을 우리가 조회하고 싶은 조건을 붙여서 만든다.
+    // 하나 또는 많이
+    // (findBy || findAllBy) + [(Column + 조건) * n] + [OrderBy + Column]
+
+
+}
