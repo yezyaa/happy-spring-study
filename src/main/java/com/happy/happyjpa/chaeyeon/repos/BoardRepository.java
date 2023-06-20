70b3d3294767c29c14b01ce7e2c568b73c559598
ycyeon <ickite@naver.com>
2023-06-20 17:38:46 +0900

    Entity 가리기. DTO 추가

--- src/main/java/com/happy/happyjpa/chaeyeon/repos/BoardRepository.java
+++ src/main/java/com/happy/happyjpa/chaeyeon/repos/BoardRepository.java
@@ -1,20 +1,20 @@
-//package com.happy.happyjpa.chaeyeon.repos;
-//
-//import com.happy.happyjpa.chaeyeon.entities.BoardEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//// 데이터베이스와 소통하기 위한 창구
-//public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-//    // JpaRepository<내가 사용할 Entity, 그 Entity의 ID 컬럼의 타입>
-//    // Spring Data JPA = Query Method
-//    // 메소드 이름을 우리가 조회하고 싶은 조건을 붙여서 만든다.
-//    // 하나 또는 많이
-//    // (findBy || findAllBy) + [(Column + 조건) * n] + [OrderBy + Column]
-//
-//    // SELECT ORDER BY
-//    // SELECT * FROM board ORDER BY id DESC;
-//    List<BoardEntity> findAllByOrderByIdDesc();
-//
-//}
+package com.happy.happyjpa.chaeyeon.repos;
+
+import com.happy.happyjpa.chaeyeon.entities.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+// 데이터베이스와 소통하기 위한 창구
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    // JpaRepository<내가 사용할 Entity, 그 Entity의 ID 컬럼의 타입>
+    // Spring Data JPA = Query Method
+    // 메소드 이름을 우리가 조회하고 싶은 조건을 붙여서 만든다.
+    // 하나 또는 많이
+    // (findBy || findAllBy) + [(Column + 조건) * n] + [OrderBy + Column]
+
+    // SELECT ORDER BY
+    // SELECT * FROM board ORDER BY id DESC;
+    List<BoardEntity> findAllByOrderByIdDesc();
+
+}
--- src/main/java/com/happy/happyjpa/yezy/AppService.java
+++ src/main/java/com/happy/happyjpa/yezy/AppService.java
@@ -1,72 +1,72 @@
-package com.happy.happyjpa.yezy;
-
-import com.happy.happyjpa.yezy.entities.BoardEntity;
-import com.happy.happyjpa.yezy.repos.BoardRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class AppService {
-    // JpaRepository
-    private final BoardRepository repository;
-
-    public AppService(BoardRepository repository) {
-        this.repository = repository;
-    }
-
-    // CREATE
-    public void createBoard(String title, String content, String name) {
-        BoardEntity newBoard = new BoardEntity();
-        newBoard.setTitle(title);
-        newBoard.setContent(content);
-        newBoard.setName(name);
-        repository.save(newBoard);
-    }
-
-    // READ
-    public BoardEntity readBoard(Long id) {
-        Optional<BoardEntity> entity = repository.findById(id);
-        if (entity.isPresent()) {
-            return entity.get();
-        } else {
-          return null;
-        }
-    }
-
-    // READALL
-//    public List<BoardEntity> readBoardAll() {
-//        return repository.findAll();
+//package com.happy.happyjpa.yezy;
+//
+//import com.happy.happyjpa.yezy.entities.BoardEntity;
+//import com.happy.happyjpa.yezy.repos.BoardRepository;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class AppService {
+//    // JpaRepository
+//    private final BoardRepository repository;
+//
+//    public AppService(BoardRepository repository) {
+//        this.repository = repository;
 //    }
-
-    // UPDATE
-    public void updateBoard(Long id, String title, String content) {
-        BoardEntity targetEntity = repository.findById(id).orElse(new BoardEntity());
-        targetEntity.setTitle(title);
-        targetEntity.setContent(content);
-        targetEntity = repository.save(targetEntity);
-        System.out.println(targetEntity);
-    }
-
-    // DELETE
-    public void deleteBoard(Long id) {
-        BoardEntity targetEntity = repository.findById(id).orElse(new BoardEntity());
-        repository.delete(targetEntity);
-    }
-
-    // findAllBy
-    public void findAllByTest() {
-        System.out.println("findAllByOrderByIdDesc");
-        List<BoardEntity> boardEntities = this.repository.findAllByOrderByIdDesc();
-        for (int i = 0; i < 5; i++) {
-            System.out.println(boardEntities.get(i));
-        }
-        System.out.println("...");
-    }
-
-    // @ResponseBody로 return
-    public List<BoardEntity> readStudentAll() {
-        return this.repository.findAll();
-    }
-}
+//
+//    // CREATE
+//    public void createBoard(String title, String content, String name) {
+//        BoardEntity newBoard = new BoardEntity();
+//        newBoard.setTitle(title);
+//        newBoard.setContent(content);
+//        newBoard.setName(name);
+//        repository.save(newBoard);
+//    }
+//
+//    // READ
+//    public BoardEntity readBoard(Long id) {
+//        Optional<BoardEntity> entity = repository.findById(id);
+//        if (entity.isPresent()) {
+//            return entity.get();
+//        } else {
+//          return null;
+//        }
+//    }
+//
+//    // READALL
+////    public List<BoardEntity> readBoardAll() {
+////        return repository.findAll();
+////    }
+//
+//    // UPDATE
+//    public void updateBoard(Long id, String title, String content) {
+//        BoardEntity targetEntity = repository.findById(id).orElse(new BoardEntity());
+//        targetEntity.setTitle(title);
+//        targetEntity.setContent(content);
+//        targetEntity = repository.save(targetEntity);
+//        System.out.println(targetEntity);
+//    }
+//
+//    // DELETE
+//    public void deleteBoard(Long id) {
+//        BoardEntity targetEntity = repository.findById(id).orElse(new BoardEntity());
+//        repository.delete(targetEntity);
+//    }
+//
+//    // findAllBy
+//    public void findAllByTest() {
+//        System.out.println("findAllByOrderByIdDesc");
+//        List<BoardEntity> boardEntities = this.repository.findAllByOrderByIdDesc();
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(boardEntities.get(i));
+//        }
+//        System.out.println("...");
+//    }
+//
+//    // @ResponseBody로 return
+//    public List<BoardEntity> readStudentAll() {
+//        return this.repository.findAll();
+//    }
+//}
