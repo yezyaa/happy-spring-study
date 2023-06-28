eccbb5b2fa61a966911bcea5d41ccb30c6741e57
ycyeon <ickite@naver.com>
2023-06-28 16:45:02 +0900

    search 실습

--- src/main/java/com/example/chaeyeon/CommentService.java
+++ src/main/java/com/example/chaeyeon/CommentService.java
@@ -1,68 +1,68 @@
-//package com.example.chaeyeon;
-//
-//import com.example.chaeyeon.dto.CommentDto;
-//import com.example.chaeyeon.entity.ArticleEntity;
-//import com.example.chaeyeon.entity.CommentEntity;
-//import com.example.chaeyeon.repository.ArticleRepository;
-//import com.example.chaeyeon.repository.CommentRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Slf4j
-//@Service
-//@RequiredArgsConstructor
-//public class CommentService {
-//    private final ArticleRepository articleRepository;
-//    private final CommentRepository commentRepository;
-//
-//    public CommentDto createComment(Long articleId, CommentDto dto) {
-//        Optional<ArticleEntity> optionalArticle
-//                = articleRepository.findById(articleId);
-//        if (optionalArticle.isEmpty())
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-//        CommentEntity newComment = new CommentEntity();
-//        newComment.setWriter(dto.getWriter());
-//        newComment.setContent(dto.getContent());
-//        newComment.setArticleId(optionalArticle.get().getId());
-//        return CommentDto.fromEntity(commentRepository.save(newComment));
-//    }
-//
-//    public List<CommentDto> readCommentAll(Long articleId) {
-//        List<CommentDto> commentList = new ArrayList<>();
-//        for (CommentEntity entity : commentRepository.findAllByArticleId(articleId)) {
-//            commentList.add(CommentDto.fromEntity(entity));
-//        }
-//        return commentList;
-//    }
-//
-//    public CommentDto updateComment(Long articleId, Long commentId, CommentDto dto) {
-//        Optional<CommentEntity> optionalComment
-//                = commentRepository.findById(commentId);
-//        if (optionalComment.isEmpty())
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-//        CommentEntity comment = optionalComment.get();
-//        if (!articleId.equals(comment.getArticleId()))
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-//
-//        comment.setContent(dto.getContent());
-//        comment.setWriter(dto.getWriter());
-//        return CommentDto.fromEntity(commentRepository.save(comment));
-//    }
-//
-//    public void deleteComment(Long articleId, Long commentId) {
-//        Optional<CommentEntity> optionalComment
-//                = commentRepository.findById(commentId);
-//        if (optionalComment.isEmpty())
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-//        CommentEntity comment = optionalComment.get();
-//
-//        commentRepository.deleteById(commentId);
-//    }
-//}
+package com.example.chaeyeon;
+
+import com.example.chaeyeon.dto.CommentDto;
+import com.example.chaeyeon.entity.ArticleEntity;
+import com.example.chaeyeon.entity.CommentEntity;
+import com.example.chaeyeon.repository.ArticleRepository;
+import com.example.chaeyeon.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+
+    public CommentDto createComment(Long articleId, CommentDto dto) {
+        Optional<ArticleEntity> optionalArticle
+                = articleRepository.findById(articleId);
+        if (optionalArticle.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        CommentEntity newComment = new CommentEntity();
+        newComment.setWriter(dto.getWriter());
+        newComment.setContent(dto.getContent());
+        newComment.setArticleId(optionalArticle.get().getId());
+        return CommentDto.fromEntity(commentRepository.save(newComment));
+    }
+
+    public List<CommentDto> readCommentAll(Long articleId) {
+        List<CommentDto> commentList = new ArrayList<>();
+        for (CommentEntity entity : commentRepository.findAllByArticleId(articleId)) {
+            commentList.add(CommentDto.fromEntity(entity));
+        }
+        return commentList;
+    }
+
+    public CommentDto updateComment(Long articleId, Long commentId, CommentDto dto) {
+        Optional<CommentEntity> optionalComment
+                = commentRepository.findById(commentId);
+        if (optionalComment.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        CommentEntity comment = optionalComment.get();
+        if (!articleId.equals(comment.getArticleId()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        comment.setContent(dto.getContent());
+        comment.setWriter(dto.getWriter());
+        return CommentDto.fromEntity(commentRepository.save(comment));
+    }
+
+    public void deleteComment(Long articleId, Long commentId) {
+        Optional<CommentEntity> optionalComment
+                = commentRepository.findById(commentId);
+        if (optionalComment.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        CommentEntity comment = optionalComment.get();
+
+        commentRepository.deleteById(commentId);
+    }
+}
--- src/main/java/com/example/yezy/ArticleService.java
+++ src/main/java/com/example/yezy/ArticleService.java
@@ -1,77 +1,77 @@
-package com.example.yezy;
-
-import com.example.yezy.dto.ArticleDto;
-import com.example.yezy.entity.ArticleEntity;
-import com.example.yezy.repository.ArticleRepository;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class ArticleService {
-    private final ArticleRepository repository;
-
-    public ArticleDto createArticle(ArticleDto dto) {
-        ArticleEntity newArticle = new ArticleEntity();
-        newArticle.setTitle(dto.getTitle());
-        newArticle.setContent(dto.getContent());
-        newArticle.setWriter(dto.getWriter());
-        return ArticleDto.fromEntity(repository.save(newArticle));
-    }
-
-    public ArticleDto readArticle(Long id) {
-        Optional<ArticleEntity> optionalArticle
-                = repository.findById(id);
-        if (optionalArticle.isPresent())
-            return ArticleDto.fromEntity(optionalArticle.get());
-        else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
-
-    public List<ArticleDto> readArticleAll() {
-        List<ArticleDto> articleList = new ArrayList<>();
-        for (ArticleEntity entity: repository.findAll()) {
-            articleList.add(ArticleDto.fromEntity(entity));
-        }
-        return articleList;
-    }
-
-    public ArticleDto updateArticle(Long id, ArticleDto dto) {
-        Optional<ArticleEntity> optionalArticle
-                = repository.findById(id);
-        if (optionalArticle.isPresent()) {
-            ArticleEntity article = optionalArticle.get();
-            article.setWriter(dto.getWriter());
-            article.setTitle(dto.getTitle());
-            article.setContent(dto.getContent());
-            repository.save(article);
-            return ArticleDto.fromEntity(article);
-        }
-        else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
-
-    public void deleteArticle(Long id) {
-        if (repository.existsById(id))
-            repository.deleteById(id);
-        else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
-
-    public List<ArticleDto> readArticlePaged() {
-        List<ArticleDto> articleDtoList = new ArrayList<>();
-        for (ArticleEntity entity : repository.findTop20ByOrderByIdDesc()) {
-            articleDtoList.add(ArticleDto.fromEntity(entity));
-        }
-        return articleDtoList;
-    }
-}
+//package com.example.yezy;
+//
+//import com.example.yezy.dto.ArticleDto;
+//import com.example.yezy.entity.ArticleEntity;
+//import com.example.yezy.repository.ArticleRepository;
+//import lombok.Data;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class ArticleService {
+//    private final ArticleRepository repository;
+//
+//    public ArticleDto createArticle(ArticleDto dto) {
+//        ArticleEntity newArticle = new ArticleEntity();
+//        newArticle.setTitle(dto.getTitle());
+//        newArticle.setContent(dto.getContent());
+//        newArticle.setWriter(dto.getWriter());
+//        return ArticleDto.fromEntity(repository.save(newArticle));
+//    }
+//
+//    public ArticleDto readArticle(Long id) {
+//        Optional<ArticleEntity> optionalArticle
+//                = repository.findById(id);
+//        if (optionalArticle.isPresent())
+//            return ArticleDto.fromEntity(optionalArticle.get());
+//        else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//    }
+//
+//    public List<ArticleDto> readArticleAll() {
+//        List<ArticleDto> articleList = new ArrayList<>();
+//        for (ArticleEntity entity: repository.findAll()) {
+//            articleList.add(ArticleDto.fromEntity(entity));
+//        }
+//        return articleList;
+//    }
+//
+//    public ArticleDto updateArticle(Long id, ArticleDto dto) {
+//        Optional<ArticleEntity> optionalArticle
+//                = repository.findById(id);
+//        if (optionalArticle.isPresent()) {
+//            ArticleEntity article = optionalArticle.get();
+//            article.setWriter(dto.getWriter());
+//            article.setTitle(dto.getTitle());
+//            article.setContent(dto.getContent());
+//            repository.save(article);
+//            return ArticleDto.fromEntity(article);
+//        }
+//        else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//    }
+//
+//    public void deleteArticle(Long id) {
+//        if (repository.existsById(id))
+//            repository.deleteById(id);
+//        else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//    }
+//
+//    public List<ArticleDto> readArticlePaged() {
+//        List<ArticleDto> articleDtoList = new ArrayList<>();
+//        for (ArticleEntity entity : repository.findTop20ByOrderByIdDesc()) {
+//            articleDtoList.add(ArticleDto.fromEntity(entity));
+//        }
+//        return articleDtoList;
+//    }
+//}
