0554dc62c7a3d078537d20980a20c3e626617394
yezy <iyeahji@gmail.com>
2023-07-10 17:05:23 +0900

    feat: @Valid 써보기 (유효성 검사) - UserDto 추가 - UserController 추가

--- src/main/java/com/happy/happyvalidation/yezy/dto/UserDto.java
+++ src/main/java/com/happy/happyvalidation/yezy/dto/UserDto.java
@@ -1,4 +1,14 @@
 package com.happy.happyvalidation.yezy.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class UserDto {
+    private Long id;
+
+    @NotBlank(message = "username is required")
+    private String username;
+    @NotBlank(message = "username is required")
+    private String email;
 }
