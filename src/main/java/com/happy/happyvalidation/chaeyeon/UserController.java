16ef2f6349e95bc4da3f4275e21fbbe751a3fb72
ycyeon <ickite@naver.com>
2023-07-10 17:10:29 +0900

    충돌방지를 위한 주석 ㅊ ㅓ리 ~

--- src/main/java/com/happy/happyvalidation/chaeyeon/UserController.java
+++ src/main/java/com/happy/happyvalidation/chaeyeon/UserController.java
@@ -1,39 +1,39 @@
-package com.happy.happyvalidation.chaeyeon;
-
-import com.happy.happyvalidation.chaeyeon.dto.UserDto;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-public class UserController {
-    @PostMapping("/users")
-    ResponseEntity<Map<String, String>> addUser(
-            @Valid @RequestBody UserDto userDto) {
-        Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("message", "success !");
-
-        return ResponseEntity.ok(responseBody);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, Object> handleValidationException(
-            MethodArgumentNotValidException exception
-    ) {
-        Map<String, Object> errors = new HashMap<>();
-        exception.getBindingResult().getFieldErrors().forEach(error ->
-        {
-            String fieldName = error.getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-
-        return errors;
-    }
-}
+//package com.happy.happyvalidation.chaeyeon;
+//
+//import com.happy.happyvalidation.chaeyeon.dto.UserDto;
+//import jakarta.validation.Valid;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//public class UserController {
+//    @PostMapping("/users")
+//    ResponseEntity<Map<String, String>> addUser(
+//            @Valid @RequestBody UserDto userDto) {
+//        Map<String, String> responseBody = new HashMap<>();
+//        responseBody.put("message", "success !");
+//
+//        return ResponseEntity.ok(responseBody);
+//    }
+//
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public Map<String, Object> handleValidationException(
+//            MethodArgumentNotValidException exception
+//    ) {
+//        Map<String, Object> errors = new HashMap<>();
+//        exception.getBindingResult().getFieldErrors().forEach(error ->
+//        {
+//            String fieldName = error.getField();
+//            String errorMessage = error.getDefaultMessage();
+//            errors.put(fieldName, errorMessage);
+//        });
+//
+//        return errors;
+//    }
+//}
