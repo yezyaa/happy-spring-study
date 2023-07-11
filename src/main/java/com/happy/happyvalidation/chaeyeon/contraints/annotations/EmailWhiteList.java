049d55857c84c8d710995180cf743ca212774326
ycyeon <ickite@naver.com>
2023-07-11 16:39:02 +0900

    feat: Blacklist, Phone010 유효성 검사 추가

---
+++ src/main/java/com/happy/happyvalidation/chaeyeon/contraints/annotations/EmailWhiteList.java
@@ -0,0 +1,18 @@
+package com.happy.happyvalidation.chaeyeon.contraints.annotations;
+
+import com.happy.happyvalidation.chaeyeon.contraints.EmailWhitelistValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+public @interface EmailWhiteList {
+    @Target({ElementType.FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Constraint(validatedBy = EmailWhitelistValidator.class)
+    public @interface EmailWhitelist {
+        String message() default "Email not in whitelist";
+        Class<?>[] groups() default {};
+        Class<? extends Payload>[] payload() default {};
+    }
+}
---
+++ src/main/java/com/happy/happyvalidation/chaeyeon/contraints/annotations/Phone010.java
@@ -0,0 +1,20 @@
+package com.happy.happyvalidation.chaeyeon.contraints.annotations;
+
+import com.happy.happyvalidation.chaeyeon.contraints.Phone010Validator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = Phone010Validator.class)
+public @interface Phone010 {
+    // Annotation Element
+    String message() default "010으로 시작하지 않음";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
