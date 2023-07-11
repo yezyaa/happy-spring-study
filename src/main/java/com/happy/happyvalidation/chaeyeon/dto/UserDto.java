61a054a0762e614f02b2d1d1a56632da71f41dcd
ycyeon <ickite@naver.com>
2023-07-11 16:39:53 +0900

    feat: Blacklist, Phone010 유효성 검사 추가 & 충돌 방지 주석 처리 . .

--- src/main/java/com/happy/happyvalidation/chaeyeon/dto/UserDto.java
+++ src/main/java/com/happy/happyvalidation/chaeyeon/dto/UserDto.java
@@ -1,13 +1,13 @@
-package com.happy.happyvalidation.chaeyeon.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class UserDto {
-    private Long id;
-
-    @NotBlank(message = "username is required")
-    private String username;
-
-    private String email;
-
-}
+//package com.happy.happyvalidation.chaeyeon.dto;
+//
+//import jakarta.validation.constraints.NotBlank;
+//
+//public class UserDto {
+//    private Long id;
+//
+//    @NotBlank(message = "username is required")
+//    private String username;
+//
+//    private String email;
+//
+//}
