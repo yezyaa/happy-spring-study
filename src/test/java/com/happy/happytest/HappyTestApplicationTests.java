09b9de79c124d9ed61bed5b1645c018ad777f43c
yezy <iyeahji@gmail.com>
2023-07-12 17:30:46 +0900

    git init

---
+++ src/test/java/com/happy/happytest/HappyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.happytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
