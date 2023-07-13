dfd06ce4e8b308d2960152274ffc551dc0157767
ycyeon <ickite@naver.com>
2023-07-13 16:46:20 +0900

    주석 .. ^^

--- src/main/java/com/happy/happytest/chaeyeon/UserRepository.java
+++ src/main/java/com/happy/happytest/chaeyeon/UserRepository.java
@@ -1,11 +1,11 @@
-package com.happy.happytest.chaeyeon;
-
-import com.example.contents.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
-    Boolean existsByUsername(String username);
-}
+//package com.happy.happytest.chaeyeon;
+//
+//import com.example.contents.entity.UserEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface UserRepository extends JpaRepository<UserEntity, Long> {
+//    Optional<UserEntity> findByUsername(String username);
+//    Boolean existsByUsername(String username);
+//}
--- src/main/java/com/happy/happytest/chaeyeon/dto/UserDto.java
+++ src/main/java/com/happy/happytest/chaeyeon/dto/UserDto.java
@@ -1,13 +1,13 @@
-package com.happy.happytest.chaeyeon.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDto {
-    private Long id;
-    private String username;
-    private String email;
-    private String phone;
-    private String bio;
-    private String avatar;
-}
+//package com.happy.happytest.chaeyeon.dto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class UserDto {
+//    private Long id;
+//    private String username;
+//    private String email;
+//    private String phone;
+//    private String bio;
+//    private String avatar;
+//}
