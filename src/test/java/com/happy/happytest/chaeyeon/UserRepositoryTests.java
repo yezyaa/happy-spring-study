97e1f377cac57df8593e0963ab27271e6144ebe4
ycyeon <ickite@naver.com>
2023-07-13 17:27:19 +0900

    feat: userSaveTest, userSaveFailTest 추가

--- src/test/java/com/happy/happytest/chaeyeon/UserRepositoryTests.java
+++ src/test/java/com/happy/happytest/chaeyeon/UserRepositoryTests.java
@@ -1,48 +1,48 @@
-package com.happy.happytest.chaeyeon;
-
-import com.happy.happytest.UserRepository;
-import com.happy.happytest.yezy.entity.UserEntity;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@DataJpaTest
-public class UserRepositoryTests {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Test
-    public void testSaveNew() {
-        // given
-        String username = "yeoon.dev";
-        UserEntity user = new UserEntity();
-        user.setUsername(username);
-
-        // when
-        user = userRepository.save(user);
-
-        // then
-        assertNotNull(user.getId());
-        assertEquals(username, user.getUsername());
-    }
-
-    @Test
-    @DisplayName("새 user 생성 실패")
-    public void testSaveNewFail() {
-        // given
-        String username = "yeoon.dev";
-        UserEntity userGiven = new UserEntity();
-        userGiven.setUsername(username);
-        userGiven = userRepository.save(userGiven);
-
-        // when
-        UserEntity user = new UserEntity();
-        user.setUsername(username);
-
-        // when-then
-        assertThrows(Exception.class, () -> userRepository.save(user));
-    }
-}
+//package com.happy.happytest.chaeyeon;
+//
+//import com.happy.happytest.UserRepository;
+//import com.happy.happytest.yezy.entity.UserEntity;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@DataJpaTest
+//public class UserRepositoryTests {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Test
+//    public void testSaveNew() {
+//        // given
+//        String username = "yeoon.dev";
+//        UserEntity user = new UserEntity();
+//        user.setUsername(username);
+//
+//        // when
+//        user = userRepository.save(user);
+//
+//        // then
+//        assertNotNull(user.getId());
+//        assertEquals(username, user.getUsername());
+//    }
+//
+//    @Test
+//    @DisplayName("새 user 생성 실패")
+//    public void testSaveNewFail() {
+//        // given
+//        String username = "yeoon.dev";
+//        UserEntity userGiven = new UserEntity();
+//        userGiven.setUsername(username);
+//        userGiven = userRepository.save(userGiven);
+//
+//        // when
+//        UserEntity user = new UserEntity();
+//        user.setUsername(username);
+//
+//        // when-then
+//        assertThrows(Exception.class, () -> userRepository.save(user));
+//    }
+//}
