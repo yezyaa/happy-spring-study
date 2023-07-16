f049494f152c2433fe018720d8fbb4a01d1d3a11
yezy <iyeahji@gmail.com>
2023-07-16 23:04:31 +0900

    feat: Profile 분리 yaml 작성 - application.yaml 추가 - application-dev.yaml 추가 - application-test.yaml 추가 - TestController 수정

--- src/main/java/com/happy/happylogging/yezy/TestController.java
+++ src/main/java/com/happy/happylogging/yezy/TestController.java
@@ -1,9 +1,23 @@
 package com.happy.happylogging.yezy;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+//@Slf4j
 @RestController
 public class TestController {
+    private static final Logger log
+            = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/log")
+    public void logTest() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("A INFO Message");
+        log.warn("A WARN Message");
+        log.error("A ERROR Message");
+    }
 }
