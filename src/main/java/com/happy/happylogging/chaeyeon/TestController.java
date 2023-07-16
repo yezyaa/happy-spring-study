a0a92c735fee1919825fd6f67292bdbfc241f22b
ycyeon <ickite@naver.com>
2023-07-16 23:04:06 +0900

    feat: profile 분리 yaml 작성

--- src/main/java/com/happy/happylogging/chaeyeon/TestController.java
+++ src/main/java/com/happy/happylogging/chaeyeon/TestController.java
@@ -1,12 +1,29 @@
-//package com.happy.happylogging.chaeyeon;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@Slf4j
-//@RestController
-//public class TestController {
-//    private static final Logger log
-//            = LoggerFactory.getLogger(TestController.class);
-//}
-//
+package com.happy.happylogging.chaeyeon;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+public class TestController {
+    private static final Logger log
+            = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/log")
+    public void logTest() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("A INFO Message");
+        log.warn("A WARN Message");
+        log.error("A ERROR Message");
+    }
+}
--- src/main/java/com/happy/happylogging/yezy/TestController.java
+++ src/main/java/com/happy/happylogging/yezy/TestController.java
@@ -1,9 +1,9 @@
-package com.happy.happylogging.yezy;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-public class TestController {
-}
+//package com.happy.happylogging.yezy;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//public class TestController {
+//}
