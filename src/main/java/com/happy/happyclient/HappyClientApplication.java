e075e0b0323242a50c7a87e599d242188529068a
yezy <iyeahji@gmail.com>
2023-08-02 21:17:15 +0900

    git init

---
+++ src/main/java/com/happy/happyclient/HappyClientApplication.java
@@ -0,0 +1,13 @@
+package com.happy.happyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happy/happyclient/HappyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.happyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
