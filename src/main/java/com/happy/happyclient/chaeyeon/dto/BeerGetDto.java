950c0a37f16957486ca79150a6e2694da320bd83
ycyeon <ickite@naver.com>
2023-08-03 00:17:57 +0900

    feat: RestTemplate GET, POST, exchange 실습

--- src/main/java/com/happy/happyclient/chaeyeon/dto/BeerGetDto.java
+++ src/main/java/com/happy/happyclient/chaeyeon/dto/BeerGetDto.java
@@ -1,4 +1,19 @@
-package com.happy.happyclient.chaeyeon.dto;
-
-public class BeerGetDto {
-}
+//package com.happy.happyclient.chaeyeon.dto;
+//
+//import lombok.Data;
+//
+//// BeerGetDto
+//@Data
+//public class BeerGetDto {
+//    private Long id;
+//    private String uid;
+//    private String brand;
+//    private String name;
+//    private String style;
+//    private String hop;
+//    private String yeast;
+//    private String malts;
+//    private String ibu;
+//    private String alcohol;
+//    private String blg;
+//}
